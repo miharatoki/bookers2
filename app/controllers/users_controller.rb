7e98e76971042f79111060d2fd8e4c099cf79789
miharatoki <kou566533@gmail.com>
2021-07-13 12:44:11 +0000

    編集画面以外のレイアウト完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -12,13 +12,13 @@ class UsersController < ApplicationController
   def index
     @book = Book.new
     @users = User.all
-    @user = current_user.
+    @user = current_user
   end
 
   def show
     @book = Book.new
     @user = User.find(params[:id])
-    @books = Book.where(id: params[:id])
+    @books = @user.books
   end
 
   def edit
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
