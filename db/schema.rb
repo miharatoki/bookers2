8052798688373e3cef83c6c6f9945b40b39d3b0d
miharatoki <kou566533@gmail.com>
2021-07-17 02:54:51 +0000

    prolile_iamgesテーブルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_13_073953) do
+ActiveRecord::Schema.define(version: 2021_07_17_024739) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -21,11 +21,6 @@ ActiveRecord::Schema.define(version: 2021_07_13_073953) do
     t.string "user_name"
   end
 
-  create_table "profile_images", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -33,10 +28,10 @@ ActiveRecord::Schema.define(version: 2021_07_13_073953) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.string "introduction"
+    t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "profile_image_id"
-    t.string "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
