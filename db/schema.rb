ccd8df1bf26cc658e86fd59103cce3beef7101bf
miharatoki <kou566533@gmail.com>
2021-07-17 09:40:29 +0000

    profile_imageカラムにデータ型を変更

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_07_11_032258) do
     t.datetime "remember_created_at"
     t.string "name"
     t.string "introduction"
-    t.integer "profile_image_id"
+    t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
